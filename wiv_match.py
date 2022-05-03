e82d39d8099c54614385a39e61b7d4b38ab432b3
Trevor Apple <trappleco@gmail.com>
2022-05-03 10:26:02 -0700

    updated script for new data formats

--- wiv_match.py
+++ wiv_match.py
@@ -27,7 +27,7 @@ def convert_csv(csv_path):
     converts csv into a list of dictionaries
     """
     data = []
-    with open(csv_path, "r") as csv_file:
+    with open(csv_path, "r", encoding="utf-8") as csv_file:
         reader = csv.reader(csv_file)
         column_names = next(reader) # NOTE: the first row has column names
         for row in reader:
@@ -38,6 +38,15 @@ def convert_csv(csv_path):
             data.append(datum)
 
     return data
+
+def make_row(mentor_datum, mentor_columns, mentee_datum, mentee_columns):
+    row = []
+    for mentor_column in mentor_columns:
+        row.append(mentor_datum.get(mentor_column, ""))
+    for mentee_column in mentee_columns:
+        row.append(mentee_datum.get(mentee_column, ""))
+
+    return row
 
 def main(argv):
     if len(argv) == 3:
@@ -116,41 +125,75 @@ def main(argv):
                     mentor_to_mentees[mentor].append(mentee_datum["Email"])
                     break
             else:
-                unmatched_mentees.append(mentee_datum["Email"])
+                unmatched_mentees.append(mentee_datum)
         else:
-            unmatched_mentees.append(mentee_datum["Email"])
+            unmatched_mentees.append(mentee_datum)
 
-    unmatched_mentors = []
-    for mentor in mentor_to_mentees:
-        mentor_datum = mentor_uid_to_data[mentor]
-        mentees = mentor_to_mentees[mentor]
-        if len(mentees) == 0:
-            unmatched_mentors.append(mentor)
-        else:
-            # NOTE: print mentor unindented first
-            print(
-                "{} {} {}".format(
-                    mentor_datum["Name (First)"],
-                    mentor_datum["Name (Middle)"],
-                    mentor_datum["Name (Last)"]
-                ),
-                mentor
-            )
-            for mentee in mentees:
-                mentee_datum = mentee_uid_to_data[mentee]
-                print(
-                    "\t{} {} {} {}".format(
-                        mentee_datum["Name (First)"],
-                        mentee_datum["Name (Middle)"],
-                        mentee_datum["Name (Last)"],
-                        mentee
-                    )
-                )
+    mentor_columns = [
+        "Name (First)",
+        "Name (Last) Email",
+        "Gender",
+        "Ethnicity",
+        "Social Media: Where can we find out more about you? LinkedIn*",
+        "Mentorship Matchup: Tell us a little about yourself so we can find you the perfect match",
+        "Country (Country)",
+        "Time Zone",
+        "What is your preferred language to use with your mentor/mentee? Is there another language that you would be comfortable using? If we can't find a match in your choice English will be the default.",
+        "Which best describes your current career stage in the voice industry",
+        "How are you involved in the industry?",
+        "How many mentees would you be willing to take on? (The expectation is only a 1-hour meeting per mentee and any further communication is entirely up to you.)",
+        "Meeting Topic: Is there anything in particular you'd like to discuss?",
+        "Anything else you would like to tell us? A request to be paired with someone with special traits?",
+        "Last Thoughts?  Work issues Public Speaking Education and experience development",
+        "Negotiation Job Hunting Networking",
+        "Crisis/Conflict Management",
+        "Portfolios and Profiles Technical/Development/Programming",
+        "Product development",
+        "Confidence building",
+        "Feeling stuck",
+        "General Professionalism",
+        "Conversational/Voice UI design",
+        "NLP"
+    ]
+    mentee_columns = [
+        "Name (First)",
+        "Name (Last)",
+        "Email",
+        "Gender",
+        "Ethnicity",
+        "Social Media: Where can we find out more about you? LinkedIn",
+        "Social Media: Where can we find out more about you? Personal Website/Portfolio",
+        "Mentorship Matchup: Tell us a little about yourself so we can find you the perfect match",
+        "Country (Country)",
+        "Time Zone",
+        "Stage: Which best describes your current career stage in the voice industry",
+        "Primary Involvement: How are you involved in the industry?",
+        "Meeting Topic: Is there anything in particular you'd like to discuss?",
+        "Anything else you would like to tell us? A request to be paired with someone with special traits?",
+        "Last thoughts?"
+    ]
+    with open('matches.csv', 'w', encoding="utf-8", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(mentor_columns + mentee_columns)
 
-    print("Unmatched mentors")
-    print(unmatched_mentors)
-    print("Unmatched mentees")
-    print(unmatched_mentees)
+        unmatched_mentors = []
+        for mentor in mentor_to_mentees:
+            mentor_datum = mentor_uid_to_data[mentor]
+            mentees = mentor_to_mentees[mentor]
+            if len(mentees) == 0:
+                unmatched_mentors.append(mentor_datum)
+            else:
+                for mentee in mentees:
+                    mentee_datum = mentee_uid_to_data[mentee]
+                    row = make_row(mentor_datum, mentor_columns, mentee_datum, mentee_columns)
+                    writer.writerow(row)
+
+        for unmatched_mentor in unmatched_mentors:
+            row = make_row(unmatched_mentor, mentor_columns, {}, mentee_columns)        
+            writer.writerow(row)
+        for unmatched_mentee in unmatched_mentees:
+            row = make_row({}, mentor_columns, unmatched_mentee, mentee_columns)        
+            writer.writerow(row)
 
 if __name__ == "__main__":
     main(sys.argv)
